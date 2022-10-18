d9ef7008ba6331e2f5fbcc7a3b1f4ec16c8672ee
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-18 17:14:53 +0000

    add function to select or get flags

---
+++ select_flag.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * select_flag - select flag for printf when a flag modifier 
+ * is found in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ *
+ * Return: 1 if a flag has been turn on, else 0
+ */
+
+ int select_flag(char s, flags_tag *f)
+ {
+    int i = 0;
+
+    switch (s)
+    {
+        case '+':
+            f->plus = 1;
+            i = 1;
+            break;
+       
+    }
+
+    return (i);
+ }
