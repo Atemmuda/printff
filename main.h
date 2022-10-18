eddeb030c47ee7d7280793d606f9d81de8351ee9
Atemmuda <yaw.ayeboahfo@gmail.com>
2022-10-18 23:24:01 +0000

    corrected select print file

--- main.h
+++ main.h
@@ -18,27 +18,23 @@ typedef struct selectPrintFn
     int (*f)(va_list ap, flags_tag *f);
 } spf;
 
-/* select_print.c */
+/* select_flag.c */
 int select_flag(char s, flags_tag *f);
-
-int _printf(const char *format, ...);
 
 int print_string(va_list list, flags_tag *f);
 int print_char(va_list list, flags_tag *f);
 
-
 int print_percent(va_list list, flags_tag *f);
 
-/*print_numbers*/
+/*print_numbers.c*/
 int print_int(va_list list, flags_tag *f);
 int count_digit(int i);
 void print_number(int n);
 
-/*write_functions*/
+/*write_functions.c*/
 int _putchar(char c);
 int _puts(char *str);
 
-
-
-
+/* The target function */
+int _printf(const char *format, ...);
 #endif
