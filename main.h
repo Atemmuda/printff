d43fc5f1f7707e5617b76dd0bd74d8fdc7448e84
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-18 17:14:14 +0000

    add prototypes to main header

--- main.h
+++ main.h
@@ -5,6 +5,32 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct flags
+{
+    int  plus;
+    int space;
+    int hash;
+} flags_tag;
+
+typedef struct selectPrintFn
+{
+    char c;
+    int (*f)(va_list ap, flags_tag *f);
+} spf;
+
+int select_flag(char s, flags_tag *f);
+
 int _printf(const char *format, ...);
+
+int print_string(va_list list, flags_tag *f);
+int print_char(va_list list, flags_tag *f);
+
+int print_percent(va_list list, flags_tag *f);
+
+int _putchar(char c);
+int _puts(char *str);
+
+
+
 
 #endif
